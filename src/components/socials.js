712563e792f0e159f5da746ac99b0c2a8d872489
JayJ104 <123anjalijain@gmail.com>
2024-07-01 01:32:25 -0700

    new footer 2.0

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,25 +1,26 @@
 import styles from "../styles/components/socials.module.scss";
 import links from "../links.json";
 
-import { RxDiscordLogo, RxInstagramLogo } from "react-icons/rx";
-import { LiaFacebook } from "react-icons/lia";
-import { MdOutlineEmail } from "react-icons/md";
+import { RiInstagramFill } from "react-icons/ri";
+import { BsDiscord } from "react-icons/bs";
+import { BiLogoFacebookCircle } from "react-icons/bi";
+import { TbMailFilled } from "react-icons/tb";
+
+const data = [
+  { platform: "discord", icon: <BsDiscord /> },
+  { platform: "instagram", icon: <RiInstagramFill /> },
+  { platform: "facebook", icon: <BiLogoFacebookCircle /> },
+  { platform: "email", icon: <TbMailFilled /> },
+];
 
 export default function Socials() {
   return (
     <div className={styles.socials}>
-      <a className={styles.a_link} target="_blank" href={links["discord"]}>
-        <RxDiscordLogo className={styles.logo} />
-      </a>
-      <a className={styles.a_link} target="_blank" href={links["instagram"]}>
-        <RxInstagramLogo className={styles.logo} />
-      </a>
-      <a className={styles.a_link} target="_blank" href={links["facebook"]}>
-        <LiaFacebook className={styles.logo} />
-      </a>
-      <a className={styles.a_link} target="_blank" href={links["email"]}>
-        <MdOutlineEmail className={styles.logo} />
-      </a>
+      {data.map((sm, index) => (
+        <a target="_blank" href={links[sm.platform]} key={index}>
+          {sm.icon}
+        </a>
+      ))}
     </div>
   );
 }
