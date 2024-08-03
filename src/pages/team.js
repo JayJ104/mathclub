0470fa407e955d85b35fc6e85e1ffb2f680c34ed
mathclub-ucdavis <mathclub@math.ucdavis.edu>
2024-08-03 01:38:01 -0400

    Update team.js

--- src/pages/team.js
+++ src/pages/team.js
@@ -20,7 +20,7 @@ export default function Team() {
                 />
               </div>
               <h2>{officer.name}</h2>
-              <c>{officer.position}</c>
+              <p className={styles.c}>{officer.position}</c>
               <p>{officer.contact}</p>
             </div>
           );
